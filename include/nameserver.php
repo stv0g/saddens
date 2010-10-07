0cab758daf113ba239ec7d7a2f16614a56bd3c6b
Steffen Vogel <info@steffenvogel.de>
2010-10-07 12:34:19 +0200

    changed cli update format added debugging statements

--- include/nameserver.php
+++ include/nameserver.php
@@ -37,22 +37,22 @@ class NameServer implements Object {
 
 	protected final function close() {
 		$output = Registry::get('output');
-	
+
 		if ($this->isRunning()) {
 			fclose($this->pipes[0]);
-				
+
 			$return['stdout'] = stream_get_contents($this->pipes[1]);
 			$return['stderr'] = stream_get_contents($this->pipes[2]);
-				
+
 			fclose($this->pipes[1]);
 			fclose($this->pipes[2]);
-				
+
 			$return['code'] = proc_close($this->process);
-				
+
 			$this->process = null;
-			
+
 			$output->add('connection to ns closed', 'debug', 1);
-				
+
 			return $return;
 		}
 		else {
@@ -63,25 +63,25 @@ class NameServer implements Object {
 	protected final function sendQueue() {
 		$output = Registry::get('output');
 		$site = Registry::get('site');
-	
+
 		if ($output->debug > 2)
 			$this->queueCommand('show');
 		$this->queueCommand('send');
 		$output->add('send queue to ns', 'debug', 1);
-		
+
 		return $this->close();
 	}
 
 	protected final function queueCommand($command) {
 		$output = Registry::get('output');
-		
+
 		if (!$this->isRunning()) {
 			$this->initialize();
 		}
 
 		fwrite($this->pipes[0], $command . "\n");
 		$this->queue[] = $command;
-		
+
 		if (substr($command, 0, 3) != 'key') {
 			$output->add('added command to ns queue', 'debug', 3, $command);
 		}
@@ -102,46 +102,47 @@ class NameServer implements Object {
 	public function query($host, $type = 'A', $class = 'IN') {
 		$output = Registry::get('output');
 		$config = Registry::get('config');
-		
+
 		$cli = 'dig -c ' . $class . ' -t ' . $type . ' ' . $host . ' @' . $this->hostname . ' +noall +answer';
 		$output->add('execute dig', 'debug', 2, $cli);
 		exec(escapeshellcmd($cli), $output, $returnCode);
-		
+
 		if ($returnCode != 0)
 			throw new NameServerException('dig query failed with code: ' . $returnCode);
 
 		$results = array();
-		
+
 		foreach ($output as $line) {
 			if (substr($line, 0, 1) != ';' && !empty($line)) {
 				$result = array();
+				$output->add('dig line', 'debug', 4, explode("\t", $line));
 				foreach (explode("\t", $line) as $column) {
 					if (!empty($column))
 					$result[] = $column;
 				}
-				
+
 				$results[] = $result;
 			}
 		}
 		return $results;
 	}
-	
+
 	/*
 	 * Output
 	 */
 	public function __toString() {
 		return $this->hostname . ':' . $this->port;
 	}
-	
+
 	public function toXml(DOMDocument $doc, $tagName = 'nameserver') {
 		$xmlNs = $doc->createElement($tagName);
-		
+
 		$xmlNs->appendChild($doc->createElement('hostname', $this->hostname));
 		$xmlNs->appendChild($doc->createElement('port', $this->port));
-		
+
 		return $xmlNs;
 	}
-	
+
 	public function toHtml() {
 		return 'nameserver: ' . $this;
 	}
